da9539561b7e5f3c251a82165469207be075ee4f
vatsal-trivedi <vatsalt@tradeix.com>
2020-02-11 09:16:15 +0000

    init

---
+++ src/main/kotlin/com/tradeix/notificationdispatchlight/NotificationDispatchLightApplication.kt
@@ -0,0 +1,11 @@
+package com.tradeix.notificationdispatchlight
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotificationDispatchLightApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotificationDispatchLightApplication>(*args)
+}
---
+++ src/test/kotlin/com/tradeix/notificationdispatchlight/NotificationDispatchLightApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tradeix.notificationdispatchlight
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotificationDispatchLightApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
